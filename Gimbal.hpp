f1220809bac82cd135c8895ae80788017373c8d4
llLeo306 <li13780602203@126.com>
2025-11-18 19:01:03 +0800

    update

--- Gimbal.hpp
+++ Gimbal.hpp
@@ -54,7 +54,7 @@ required_hardware:
   - bmi088
 depends:
   - qdu-future/BMI088
-  - qdu-future/Motor
+  - qdu-future/RMMotor
   - xrobot-org/MadgwickAHRS
 === END MANIFEST === */
 // clang-format on
